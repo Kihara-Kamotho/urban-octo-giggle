72cc7851592b04cf79264487cbcca263ef5d5e25
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-07-22 20:50:58 +0300

    Remove cols course_id and lesson_id from lessons

--- db/migrate/20230722174025_remove_course_from_lessons.rb
+++ db/migrate/20230722174025_remove_course_from_lessons.rb
@@ -1,5 +1,6 @@
 class RemoveCourseFromLessons < ActiveRecord::Migration[7.0]
   def change
     remove_column :lessons, :lesson_id, :integer
+    remove_column :lessons, :course_id, :integer
   end
 end
