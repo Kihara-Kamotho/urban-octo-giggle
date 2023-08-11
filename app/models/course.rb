95c4b2111f0626b0466178a7e13a19522a20df4c
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-08-11 14:48:50 +0300

    Add cart courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
   has_many :sections
   has_many :lessons, through: :sections
+  has_and_belongs_to_many :carts
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }
 end
