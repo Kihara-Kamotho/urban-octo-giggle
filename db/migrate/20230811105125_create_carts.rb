5b27e8a373c882e61651ad7d005e25122598dac0
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-08-12 11:09:53 +0300

    Add defaults to cart

--- db/migrate/20230811105125_create_carts.rb
+++ db/migrate/20230811105125_create_carts.rb
@@ -2,7 +2,7 @@ class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts do |t|
       t.integer :amount, default: 0
-      t.boolean :status, default: true
+      t.boolean :status, default: false
 
       t.timestamps
     end
